a86a88cdcd6618f33e0b89fcfca135a08e48bb8f
Kaviya <kaviyasugumar8@gmail.com>
2026-03-05 15:29:38 +0530

    UC14: Add Temperature measurement with selective arithmetic support

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -4,37 +4,28 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        Quantity<LengthUnit> length1 =
-                new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<TemperatureUnit> t1 =
+                new Quantity<>(0.0, TemperatureUnit.CELSIUS);
 
-        Quantity<LengthUnit> length2 =
-                new Quantity<>(6.0, LengthUnit.INCH);
+        Quantity<TemperatureUnit> t2 =
+                new Quantity<>(32.0, TemperatureUnit.FAHRENHEIT);
 
-        System.out.println("Add: " + length1.add(length2));
-        System.out.println("Subtract: " + length1.subtract(length2));
-        System.out.println("Divide: " +
-                length1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
+        System.out.println("Temperature Equal: " + t1.equals(t2));
 
-        Quantity<WeightUnit> weight1 =
-                new Quantity<>(10.0, WeightUnit.KILOGRAM);
+        System.out.println(
+                new Quantity<>(100.0, TemperatureUnit.CELSIUS)
+                        .convertTo(TemperatureUnit.FAHRENHEIT)
+        );
 
-        Quantity<WeightUnit> weight2 =
-                new Quantity<>(5000.0, WeightUnit.GRAM);
+        try {
 
-        System.out.println("Weight Add: " + weight1.add(weight2));
-        System.out.println("Weight Subtract: " + weight1.subtract(weight2));
-        System.out.println("Weight Divide: " +
-                weight1.divide(new Quantity<>(5.0, WeightUnit.KILOGRAM)));
+            new Quantity<>(100.0, TemperatureUnit.CELSIUS)
+                    .add(new Quantity<>(50.0, TemperatureUnit.CELSIUS));
 
-        Quantity<VolumeUnit> volume1 =
-                new Quantity<>(5.0, VolumeUnit.LITRE);
+        } catch (Exception e) {
 
-        Quantity<VolumeUnit> volume2 =
-                new Quantity<>(500.0, VolumeUnit.MILLILITRE);
+            System.out.println(e.getMessage());
 
-        System.out.println("Volume Add: " + volume1.add(volume2));
-        System.out.println("Volume Subtract: " + volume1.subtract(volume2));
-        System.out.println("Volume Divide: " +
-                volume1.divide(new Quantity<>(1.0, VolumeUnit.LITRE)));
+        }
     }
 }
