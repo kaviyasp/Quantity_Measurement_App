7dee6892284ccb5dd118e697a1dd7feacddb6385
Kaviya <kaviyasugumar8@gmail.com>
2026-03-05 15:23:37 +0530

    UC13: Refactor arithmetic logic using centralized helper to enforce DRY principle

--- src/main/java/com/bridgelabz/QuantityMeasurementApp.java
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -10,10 +10,9 @@ public class QuantityMeasurementApp {
         Quantity<LengthUnit> length2 =
                 new Quantity<>(6.0, LengthUnit.INCH);
 
-        System.out.println("Subtraction: " +
-                length1.subtract(length2));
-
-        System.out.println("Division: " +
+        System.out.println("Add: " + length1.add(length2));
+        System.out.println("Subtract: " + length1.subtract(length2));
+        System.out.println("Divide: " +
                 length1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
 
         Quantity<WeightUnit> weight1 =
@@ -22,10 +21,9 @@ public class QuantityMeasurementApp {
         Quantity<WeightUnit> weight2 =
                 new Quantity<>(5000.0, WeightUnit.GRAM);
 
-        System.out.println("Weight subtraction: " +
-                weight1.subtract(weight2));
-
-        System.out.println("Weight division: " +
+        System.out.println("Weight Add: " + weight1.add(weight2));
+        System.out.println("Weight Subtract: " + weight1.subtract(weight2));
+        System.out.println("Weight Divide: " +
                 weight1.divide(new Quantity<>(5.0, WeightUnit.KILOGRAM)));
 
         Quantity<VolumeUnit> volume1 =
@@ -34,10 +32,9 @@ public class QuantityMeasurementApp {
         Quantity<VolumeUnit> volume2 =
                 new Quantity<>(500.0, VolumeUnit.MILLILITRE);
 
-        System.out.println("Volume subtraction: " +
-                volume1.subtract(volume2));
-
-        System.out.println("Volume division: " +
+        System.out.println("Volume Add: " + volume1.add(volume2));
+        System.out.println("Volume Subtract: " + volume1.subtract(volume2));
+        System.out.println("Volume Divide: " +
                 volume1.divide(new Quantity<>(1.0, VolumeUnit.LITRE)));
     }
 }
